23ddb47f581fbf7eaee52c9761770aee0cd0eeb3
Diana-Petruccetti <dianapetruccetti@gmail.com>
2024-10-21 16:08:50 +0200

    cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,13 +15,29 @@ sfruttando la risposta per generare dinamicamente in pagina una serie di foto!
 Bonus
 rendi la pagina responsive, in modo che su mobile e tablet le foto si dispongano man mano una sotto l’altra
 ed il titolo abbia una dimensione adeguata */
-let rowEl = document.querySelector(".row");
+const rowEl = document.querySelector('.row')
+axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
+    .then(response => {
 
-for (let i = 0; i < 1; i++){
-    axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
-        .then((response) => {
-            console.log(response.data)
+        const row = response.data
+        console.log(row);
 
-            document.querySelector('paper');
-    })
-}
+        let rowElements = ''
+        row.forEach(row => {
+            const { url, title } = row
+
+            const markup = `
+            <div class="col-4 mt-5">
+                <div class="paper container">
+                    <img class="pt-3" src="${url}" width="100%" height="" alt="">
+                    <p class="mt-3 pb-4">${title}</p>
+                </div>
+            </div>
+            `
+            rowElements += markup
+
+        })
+
+        rowEl.innerHTML = rowElements
+
+    }).catch(err => console.error(err))
