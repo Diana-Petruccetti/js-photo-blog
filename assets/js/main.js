51ddad39598df3971348ee5de5f6830beda47300
Diana-Petruccetti <dianapetruccetti@gmail.com>
2024-10-21 12:27:51 +0200

    working on cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,19 +18,10 @@ ed il titolo abbia una dimensione adeguata */
 let rowEl = document.querySelector(".row");
 
 for (let i = 0; i < 1; i++){
-    axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then((response) => {
-        console.log(response.data)
-        let card = axios[i];
-        console.log(card);
-        let {url, title} = card;
-        let markup = `
-            <div class="col-4 mt-5">
-                <div class="paper container">
-                    <img class="pt-3" src="${url}" width="100%" height="" alt="">
-                    <p class="mt-3 pb-4">${title}</p>
-                </div>
-            </div>
-        `
-        rowEl.innerHTML += markup;
+    axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
+        .then((response) => {
+            console.log(response.data)
+
+            document.querySelector('paper');
     })
 }
